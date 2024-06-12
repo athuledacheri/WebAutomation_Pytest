b330dff302f7d6c8b3ea3b32bd8fe2fc592dd7d4
athul.e <athulashok529@gmail.com>
2024-06-12 14:32:52 +0530

    Third commit

--- PageObjects/LoginPage.py
+++ PageObjects/LoginPage.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
 
+
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
+        self.title = (By.XPATH, "//span[@class='title']")
+        self.addToCart = (By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
 
     def enter_username(self, username):
         self.driver.find_element(*self.username_field).send_keys(username)
@@ -15,3 +18,10 @@ class LoginPage:
 
     def click_login_button(self):
         self.driver.find_element(*self.login_button).click()
+
+    def VerifyTitle(self):
+        textMatch = self.driver.find_element(*self.title).text
+        assert ("Products" in textMatch)
+
+    def AddToCart(self):
+        self.driver.find_element(*self.addToCart).click()
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -7,14 +7,11 @@ from Utilities.ExcelUtils import get_data_from_excel
 @pytest.mark.parametrize("username, password", get_data_from_excel("testData.xlsx", "LoginData"))
 class TestSample(BaseClass):
     def test_e2e(self, username, password):
-        # Create an instance of the LoginPage
         login_page = LoginPage(self.driver)
 
-        # Enter username and password
         login_page.enter_username(username)
         login_page.enter_password(password)
 
-        # Click the login button
         login_page.click_login_button()
-
-        # You can add assertions here to verify the login was successful
+        login_page.VerifyTitle()
+        login_page.AddToCart()
